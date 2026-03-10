7bab9e3bd4b263165b52b3ac7d2c3a13ce64e903
Nived <nivedsuresh5@gmail.com>
2026-03-10 22:45:24 +0530

    just

--- front-end/src/features/admin/order/hooks/useNewOrders.js
+++ front-end/src/features/admin/order/hooks/useNewOrders.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import api from '../../../../api/axios'; // അല്ലെങ്കിൽ നിങ്ങളുടെ axios instance
+import api from '../../../../api/axios'; 
 import { toast } from 'sonner';
 import { extractErrorMessages } from '../../../../utils/extractErrorMessages';
 
