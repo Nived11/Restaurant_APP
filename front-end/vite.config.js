7afc81b1281b8b3a30bbec8d81c59f4a95a9e4b9
Nived <nivedsuresh5@gmail.com>
2026-03-11 00:36:32 +0530

    small changes in ..

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -10,22 +10,30 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'THE CRUNCH',
+        name: 'THE CRUNCH INDIA',
+        short_name: 'THE CRUNCH', 
         theme_color: '#1a1a1a', 
-        background_color: '#ffffff',
+        background_color: '#1a1a1a', 
         display: 'standalone',
+        scope: '/',
+        start_url: '/',
         icons: [
           {
-            src: '/icon-192.png',
+            src: 'icon-192.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
-            src: '/icon-512.png',
+            src: 'icon-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,mp3}'],
       }
     })
   ],
