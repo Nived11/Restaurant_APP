6fefae0e6dc7b448f9a2d0350e8e953976e5ff8d
Nived <nivedsuresh5@gmail.com>
2026-03-14 19:14:37 +0530

    Global Error is done

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -12,29 +12,29 @@ export default defineConfig({
       manifest: {
         name: 'THE CRUNCH INDIA',
         short_name: 'Crunch', 
-        theme_color: '#1a1a1a', 
+        theme_color: '#ffffff', 
         background_color: '#ffffff', 
         display: 'standalone',
         scope: '/',
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
+            src: '/icon-192.png', 
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
-            src: 'icon-512.png',
+            src: '/icon-512.png', 
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           }
         ]
       },
       workbox: {
        maximumFileSizeToCacheInBytes: 20 * 1024 * 1024, 
-      globPatterns: ['**/*.{js,css,html,png,svg,mp3}'],
+       globPatterns: ['**/*.{js,css,html,png,svg,mp3}'],
       }
     })
   ],
